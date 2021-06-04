97a7bd670e99f9b00b8062cd0d4e5c0ce71f4d55
dengshilin <dengshilin@gdcattsoft.com>
2021-06-04 17:20:12 +0800

    日期格式化问题

--- packages/d2-crud-plus/src/lib/types/list/date.js
+++ packages/d2-crud-plus/src/lib/types/list/date.js
@@ -2,11 +2,11 @@ import { daterangeFormatter, datetimerangeFormatter, shortcuts } from '../functi
 
 export default {
   datetime: {
-    form: { component: { name: 'el-date-picker', props: { type: 'datetime' } } },
+    form: { component: { name: 'el-date-picker', props: { type: 'datetime', 'value-format': 'yyyy-MM-dd HH:mm:ss' } } },
     component: { name: 'date-format' }
   },
   date: {
-    form: { component: { name: 'el-date-picker' } },
+    form: { component: { name: 'el-date-picker', props: { type: 'date', 'value-format': 'YYYY-MM-DD' } } },
     component: { name: 'date-format', props: { format: 'YYYY-MM-DD' } }
   },
   daterange: {
@@ -18,7 +18,8 @@ export default {
           'range-separator': '至',
           'start-placeholder': '开始',
           'end-placeholder': '结束',
-          'picker-options': { shortcuts: shortcuts }
+          'picker-options': { shortcuts: shortcuts },
+          'value-format': 'YYYY-MM-DD'
         }
       }
     },
@@ -33,14 +34,15 @@ export default {
           'range-separator': '至',
           'start-placeholder': '开始',
           'end-placeholder': '结束',
-          'picker-options': { shortcuts: shortcuts }
+          'picker-options': { shortcuts: shortcuts },
+          'value-format': 'YYYY-MM-DD HH:mm:ss'
         }
       }
     },
     formatter: datetimerangeFormatter
   },
   time: {
-    form: { component: { name: 'el-time-picker' } },
+    form: { component: { name: 'el-time-picker', props: { 'value-format': 'HH:mm:ss' } } },
     component: { name: 'date-format', props: { format: 'HH:mm:ss' } }
   }
 }
